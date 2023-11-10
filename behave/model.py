46808935208a10eb301fb6e73f4e23097921b743
周伟 <wei.zhou@matrixport.com>
2023-11-10 12:32:16 +0800

    fix

--- behave/model.py
+++ behave/model.py
@@ -1180,7 +1180,7 @@ class Scenario(TagAndStatusStatement, Replayable):
         scenario_str = str(self)
         # print("====>scenario start", scenario_str)
 
-        if not getattr(runner.config, "check_previous", False) and check_string_in_file(
+        if getattr(runner.config, "check_previous", False) and check_string_in_file(
             scenario_str
         ):
             print("=====>scenario passed previously", scenario_str)
